0e2077a5b1db45e9212627f6de8829647a7f3837
Anakor Joshua <becomingadev@gmail.com>
2024-06-27 12:05:40 +0100

    Create account recovery page

---
+++ app/(auth)/account-recovery/page.tsx
@@ -0,0 +1,32 @@
+function AccountRecovery() {
+  return (
+    <section className="text-center">
+      <header className="mb-[69px]">
+        <h1 className="font-black text-[70px] leading-[85.33px]">
+          Account Recovery!
+        </h1>
+      </header>
+
+      <main className="mb-[132px] max-w-[842px] text-[32px]">
+        <p>
+          Do you want to recover your account Click Yes button to recover Click
+          No button to delete account permanently
+        </p>
+        <p>There are seven days left for account deletion</p>
+      </main>
+
+      <footer className="flex gap-x-[6.938rem] justify-center">
+        <button className="bg-[#AB2929] text-white rounded-[30px] px-[4.3rem] py-[1.688rem] text-[2.813rem] leading-[3.429rem] font-extrabold">
+          Deleted
+        </button>
+        <button className="bg-[#0077B6] text-white rounded-[30px] px-[4.3rem] py-[1.688rem] text-[2.813rem] leading-[3.429rem] font-extrabold">
+          Continue
+        </button>
+      </footer>
+    </section>
+  );
+  1;
+}
+11;
+
+export default AccountRecovery;
