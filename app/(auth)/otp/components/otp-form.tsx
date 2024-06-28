ab4db2c6d8f0975ae7c763c854aafc23479ad02f
Anakor Joshua <becomingadev@gmail.com>
2024-06-28 14:25:39 +0100

    Integrate otp api

--- app/(auth)/otp/components/otp-form.tsx
+++ app/(auth)/otp/components/otp-form.tsx
@@ -1,68 +1,86 @@
 "use client";
-
-import { ErrorMessage, Field, Form, Formik } from "formik";
 import Link from "next/link";
-import { toFormikValidationSchema } from "zod-formik-adapter";
-import { OTPFormValidationSchema } from "../lib/validate";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 function OTPForm() {
+  const router = useRouter();
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
+  const [num3, setNum3] = useState("");
+  const [num4, setNum4] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const otp = num1 + num2 + num3 + num4;
+
+    fetch("https://tast-pwvf.onrender.com/user/confirmcode", {
+      method: "POST",
+      body: JSON.stringify({ code: otp }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then((res) =>
+      res.json().then((data) => {
+        console.log(data);
+
+        if (data.status && data.status_code === 200) {
+          router.push("/success-signup");
+        } else {
+          setErrMsg(data.message);
+        }
+      })
+    );
+  };
+
   return (
-    <Formik
-      initialValues={{
-        num1: "",
-        num2: "",
-        num3: "",
-        num4: "",
-      }}
-      validationSchema={toFormikValidationSchema(OTPFormValidationSchema)}
-      onSubmit={async (values, { setSubmitting }) => {
-        console.log(values);
+    <form>
+      {errMsg && <p className="mb-2 text-red-500">{errMsg}</p>}
+      <div className="flex justify-center  sm:gap-x-6 2xl:gap-x-[3.063rem] max-w-[714px] mx-auto">
+        <input
+          name="num1"
+          type="number"
+          className="rounded-40 py-4 px-6 text-xl font-extrabold focus:bg-[#CDEEFF80] focus:border-[#0077B6] bg-[#2F2F2F1A] w-[70px] 2xl:w-[120px] 2xl:py-10 text-center rounded-xl focus:outline-none focus:border"
+          onChange={(e) => setNum1(e.target.value)}
+        />
 
-        setSubmitting(false);
-      }}
-    >
-      <Form>
-        <div className="flex justify-center  sm:gap-x-6 2xl:gap-x-[3.063rem] max-w-[714px] mx-auto">
-          <Field
-            name="num1"
-            type="number"
-            className="rounded-40 py-4 px-6 text-xl font-extrabold focus:bg-[#CDEEFF80] focus:border-[#0077B6] bg-[#2F2F2F1A] w-[70px] 2xl:w-[120px] 2xl:py-10 text-center rounded-xl focus:outline-none focus:border"
-          />
+        <input
+          name="num2"
+          type="number"
+          className="rounded-40 py-4 px-6 text-xl font-extrabold focus:bg-[#CDEEFF80] focus:border-[#0077B6] bg-[#2F2F2F1A] w-[70px] 2xl:w-[120px] 2xl:py-10 text-center rounded-xl focus:outline-none focus:border"
+          onChange={(e) => setNum2(e.target.value)}
+        />
+        <input
+          name="num3"
+          type="number"
+          className="rounded-40 py-4 px-6 text-xl font-extrabold focus:bg-[#CDEEFF80] focus:border-[#0077B6] bg-[#2F2F2F1A] w-[70px] 2xl:w-[120px] 2xl:py-10 text-center rounded-xl focus:outline-none focus:border"
+          onChange={(e) => setNum3(e.target.value)}
+        />
+        <input
+          name="num4"
+          type="number"
+          className="rounded-40 py-4 px-6 text-xl font-extrabold focus:bg-[#CDEEFF80] focus:border-[#0077B6] bg-[#2F2F2F1A] w-[70px] 2xl:w-[120px] 2xl:py-10 text-center rounded-xl focus:outline-none focus:border"
+          onChange={(e) => setNum4(e.target.value)}
+        />
+      </div>
 
-          <Field
-            name="num2"
-            type="number"
-            className="rounded-40 py-4 px-6 text-xl font-extrabold focus:bg-[#CDEEFF80] focus:border-[#0077B6] bg-[#2F2F2F1A] w-[70px] 2xl:w-[120px] 2xl:py-10 text-center rounded-xl focus:outline-none focus:border"
-          />
-          <Field
-            name="num3"
-            type="number"
-            className="rounded-40 py-4 px-6 text-xl font-extrabold focus:bg-[#CDEEFF80] focus:border-[#0077B6] bg-[#2F2F2F1A] w-[70px] 2xl:w-[120px] 2xl:py-10 text-center rounded-xl focus:outline-none focus:border"
-          />
-          <Field
-            name="num4"
-            type="number"
-            className="rounded-40 py-4 px-6 text-xl font-extrabold focus:bg-[#CDEEFF80] focus:border-[#0077B6] bg-[#2F2F2F1A] w-[70px] 2xl:w-[120px] 2xl:py-10 text-center rounded-xl focus:outline-none focus:border"
-          />
-        </div>
+      <div className="flex flex-col items-center gap-y-2 justify-center 2xl:mt-[6.063rem] mt-12">
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          className="bg-[#0077B6] text-white rounded-[30px] 2xl:px-[5.813rem] 2xl:py-[1.688rem] 2xl:text-2xl text-xl px-16 py-4 2xl:leading-[3.429rem] font-extrabold"
+        >
+          Verify
+        </button>
 
-        <div className="flex flex-col items-center gap-y-2 justify-center 2xl:mt-[6.063rem] mt-12">
-          <Link href="">
-            <button
-              type="submit"
-              className="bg-[#0077B6] text-white rounded-[30px] 2xl:px-[5.813rem] 2xl:py-[1.688rem] 2xl:text-2xl text-xl px-16 py-4 2xl:leading-[3.429rem] font-extrabold"
-            >
-              Verify
-            </button>
-          </Link>
-
-          <p>
-            Didn’t receive code?{" "}
-            <button className="font-bold">Resend Again</button>.{" "}
-          </p>
-        </div>
-      </Form>
-    </Formik>
+        <p>
+          Didn’t receive code?{" "}
+          <button className="font-bold">Resend Again</button>.{" "}
+        </p>
+      </div>
+    </form>
   );
 }
 
