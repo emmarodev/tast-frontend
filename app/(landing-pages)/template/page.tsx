76abc93c331f6c58399cd628dfba9d51c233e938
Emmaro <emmarodev@gmail.com>
2024-07-04 15:18:24 +0100

    Add images to template page

--- app/(landing-pages)/template/page.tsx
+++ app/(landing-pages)/template/page.tsx
@@ -4,6 +4,20 @@ import { CiHeart } from "react-icons/ci";
 import { IoMdPricetag, IoMdShare } from "react-icons/io";
 import { IoEyeSharp } from "react-icons/io5";
 import { Footer } from "../../page";
+import team from "../../../public/team.jpg";
+import rally from "../../../public/rally.jpg";
+import estate from "../../../public/estate.jpg";
+import discuss from "../../../public/discuss.jpg";
+import cloud from "../../../public/cloud.jpg";
+import camp from "../../../public/camp.jpg";
+import brain from "../../../public/brain.jpg";
+import scifi from "../../../public/scifi.jpg";
+import angular from "../../../public/icons/angular.svg";
+import figma from "../../../public/icons/figma.svg";
+import adobe from "../../../public/icons/adobe.svg";
+import Image from "next/image";
+import { SiNestjs, SiTailwindcss } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
 
 function Template() {
   return (
@@ -29,21 +43,21 @@ const Hero = () => {
   return (
     <section className="sm:py-20 relative bg-gray-500">
       <div className="w-full h-full sm:flex items-center text-white">
-        <div className="hidden sm:block">
-          {/* <Image
-              alt="Mountains"
-              src={}
-              placeholder="blur"
-              quality={100}
-              layout="fill"
-              sizes="100vw"
-              style={{
-                objectFit: "cover",
-                zIndex: -5,
-              }}
-            /> */}
-        </div>
-        <div className="rounded-none sm:rounded-[40px] 2xl:pl-[5.313rem] 2xl:pr-[7.25rem] 2xl:py-[4.625rem] px-5 sm:px-20 py-8 flex gap-y-5 flex-col items-start">
+        <Image
+          alt="Mountains"
+          src={scifi}
+          placeholder="blur"
+          quality={100}
+          layout="fill"
+          sizes="100vw"
+          style={{
+            objectFit: "cover",
+            zIndex: 0,
+          }}
+        />
+
+        <div className="z-10 absolute w-full h-full top-0 left-0 bg-black/70"></div>
+        <div className="rounded-none sm:rounded-[40px] 2xl:pl-[5.313rem] 2xl:pr-[7.25rem] 2xl:py-[4.625rem] px-5 sm:px-20 py-8 flex gap-y-5 flex-col items-start z-20">
           <Line />
           <h1 className="text-3xl max-w-[740px] font-bold leading-[60px] capitalize">
             our project And
@@ -68,14 +82,30 @@ const Hero = () => {
 const Tools = () => {
   return (
     <div className="flex justify-center items-center gap-x-5">
-      <div className="h-16 w-16 rounded-full bg-[#330000]"></div>
-      <div className="h-16 w-16 rounded-full bg-[#330000]"></div>
-      <div className="h-16 w-16 rounded-full bg-[#330000]"></div>
-      <div className="h-16 w-16 rounded-full bg-[#330000]"></div>
-      <div className="h-16 w-16 rounded-full bg-[#330000]"></div>
-      <div className="h-16 w-16 rounded-full bg-[#330000]"></div>
-      <div className="h-16 w-16 rounded-full bg-[#330000]"></div>
-      <div className="h-16 w-16 rounded-full bg-[#330000]"></div>
+      <div className="h-16 w-16 rounded-full bg-[#330000] flex items-center justify-center">
+        <FaReact className="text-blue-500 text-5xl" />
+      </div>
+      <div className="h-16 w-16 rounded-full bg-blue-900 flex items-center justify-center text-4xl font-bold text-blue-400">
+        Ps
+      </div>
+      <div className="h-16 w-16 rounded-full bg-[#330000] text-pink-600 flex items-center justify-center">
+        <SiNestjs className="text-5xl" />
+      </div>
+      <div className="h-16 w-16 rounded-full bg-[#330000] flex items-center justify-center text-4xl font-bold text-yellow-600">
+        Ai
+      </div>
+      <div className="h-16 w-16 rounded-full bg-[#330000] flex items-center justify-center">
+        <Image src={angular} alt={"angular icon"} width={50} />
+      </div>
+      <div className="h-16 w-16 rounded-full bg-[#330000] flex items-center justify-center">
+        <Image src={figma} alt={"figma icon"} width={30} height={40} />
+      </div>
+      <div className="h-16 w-16 rounded-full bg-[#330000] flex items-center justify-center">
+        <Image src={adobe} alt={"adobe icon"} width={40} height={40} />
+      </div>
+      <div className="h-16 w-16 rounded-full bg-[#330000] flex items-center justify-center">
+        <SiTailwindcss className="text-blue-500 text-5xl" />
+      </div>
     </div>
   );
 };
@@ -84,27 +114,39 @@ const Project = () => {
   return (
     <section>
       <div className="grid grid-cols-4 gap-4">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        <ProjectCard image={camp} />
+        <ProjectCard image={discuss} />
+        <ProjectCard image={brain} />
+        <ProjectCard image={team} />
+        <ProjectCard image={cloud} />
+        <ProjectCard image={rally} />
+        <ProjectCard image={camp} />
+        <ProjectCard image={estate} />
+        <ProjectCard image={cloud} />
+        <ProjectCard image={rally} />
+        <ProjectCard image={camp} />
+        <ProjectCard image={estate} />
       </div>
     </section>
   );
 };
 
-const ProjectCard = () => {
+const ProjectCard = ({ image }: { image: any }) => {
   return (
     <article className="bg-white rounded-xl shadow-xl px-6 py-5 text-[#00000099] font-medium relative overflow-hidden">
-      <header className="bg-gray-500 rounded-lg h-52 mb-4"></header>
+      <header className="rounded-lg h-52 mb-4 relative overflow-hidden">
+        <Image
+          alt="Mountains"
+          src={image}
+          placeholder="blur"
+          quality={100}
+          layout="fill"
+          sizes="100vw"
+          style={{
+            objectFit: "cover",
+          }}
+        />
+      </header>
       <main className="mb-2">
         <p className="tracking-wide text-sm">NOV. 12, 2024</p>
         <h3 className="font-bold text-xl text-black mb-2">Web Development</h3>
