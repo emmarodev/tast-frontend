0e8690ef3b1ea39a8b920bc96dd69f1fcbd79787
Emmaro <emmarodev@gmail.com>
2024-08-04 20:13:10 +0100

    Create order page modal

--- app/dashboard/orders/client-order-page.tsx
+++ app/dashboard/orders/client-order-page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 import Card from "../components/Card";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import Image from "next/image";
+import { GiCheckMark } from "react-icons/gi";
 
 export default function OrderPage({ data }: { data: any }) {
   const [showPaymentModal, setShowPaymentModal] = useState(false);
+  const [showVerifyPaymentModal, setShowVerifyPaymentModal] = useState(false);
+  const [successPaymentModal, setSuccessPaymentModal] = useState(false);
 
   const cardTitles = [
     "Total Orders",
@@ -81,9 +85,12 @@ export default function OrderPage({ data }: { data: any }) {
                 </button>
               </td>
               <td className="border-r border-r-[#FFB200]">
-                <span className="rounded bg-[#FF7777] px-3 py-1 text-white">
+                <button
+                  className="rounded bg-[#FF7777] px-3 py-1 text-white"
+                  onClick={() => setShowPaymentModal(true)}
+                >
                   Pending
-                </span>
+                </button>
               </td>
               <td>
                 <button className="rounded bg-[#FFB200] px-3 py-2 text-sm font-semibold uppercase">
@@ -112,14 +119,246 @@ export default function OrderPage({ data }: { data: any }) {
       </footer>
 
       {showPaymentModal && (
-        <article>
-          <header></header>
-          <main>
-            <div></div>
-            <div></div>
-          </main>
-        </article>
+        <div className="absolute left-0 top-0 h-screen w-screen bg-black/50">
+          <article className="absolute left-1/2 top-1/2 w-3/5 -translate-x-1/2 -translate-y-1/2 bg-white">
+            <header className="border py-5 shadow-lg">
+              <form
+                action={"somthing"}
+                className="flex items-center justify-center gap-x-5"
+              >
+                <div>
+                  {/* <label
+                    htmlFor="pay_method"
+                    className="mb-1 block px-4 text-xs"
+                  >
+                    Payment Method
+                  </label> */}
+                  <select className="rounded-full border-2 border-[#1497CF] px-4 py-3 text-sm">
+                    <option value="">Payment method</option>
+                  </select>
+                </div>
+                <div>
+                  {/* <label
+                    htmlFor="pay_method"
+                    className="mb-1 block px-4 text-xs"
+                  >
+                    Select Wallet
+                  </label> */}
+                  <select className="rounded-full border-2 border-[#1497CF] px-4 py-3 text-sm">
+                    <option value="">Select wallet</option>
+                  </select>
+                </div>
+                <div>
+                  {/* <label
+                    htmlFor="pay_method"
+                    className="mb-1 block px-4 text-xs"
+                  >
+                    Payment Amount
+                  </label> */}
+                  <input
+                    className="rounded-full border-2 border-[#1497CF] px-4 py-3 text-sm placeholder:text-black"
+                    placeholder="Payment Amount"
+                  />
+                </div>
+              </form>
+            </header>
+
+            <main className="flex items-center justify-between gap-x-10 px-8 py-6">
+              <div className="flex grow items-center justify-between gap-x-6">
+                <Image
+                  src="/images/payoneer.png"
+                  alt="Payoneer icon"
+                  width={150}
+                  height={80}
+                />
+                <div className="text-sm">
+                  <p>Bank: Payoneer</p>
+                  <p>Account Name: Parsonal</p>
+                  <p>Account: 494949449</p>
+                  <p>Routing: SD46649646</p>
+                </div>
+                <Image
+                  src="/images/qr-code.png"
+                  alt="A QR code"
+                  width={80}
+                  height={80}
+                />
+              </div>
+              <PaymentInformation
+                handleClick={() => {
+                  setShowPaymentModal(false);
+                  setShowVerifyPaymentModal(true);
+                }}
+              />
+            </main>
+            <footer>
+              <p className="py-4 text-center text-sm font-semibold text-red-700">
+                Click on continue after you have paid the specified amount to
+                the bank info provided here
+              </p>
+            </footer>
+          </article>
+        </div>
+      )}
+
+      {showVerifyPaymentModal && (
+        <div className="absolute left-0 top-0 h-screen w-screen bg-black/50">
+          <article className="absolute left-1/2 top-1/2 max-h-[90vh] w-3/5 -translate-x-1/2 -translate-y-1/2 overflow-y-auto bg-white p-5">
+            <h2 className="mb-8 text-2xl font-bold">
+              Enter your Mobile Wallet Information
+            </h2>
+
+            <div className="flex items-center gap-x-10">
+              <form action={"something"} className="grid grow gap-y-4">
+                <div>
+                  <label
+                    htmlFor="ahn"
+                    className="mb-1 block text-sm font-medium"
+                  >
+                    Account Holder Name
+                  </label>
+                  <input
+                    type="text"
+                    id="ahn"
+                    className="border-[#00000026 w-full rounded-lg border bg-[#D9D9D91A] px-4 py-3 text-sm"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="mwn"
+                    className="mb-1 block text-sm font-medium"
+                  >
+                    Mobile Wallet Name
+                  </label>
+                  <input
+                    type="text"
+                    id="mwn"
+                    className="border-[#00000026 w-full rounded-lg border bg-[#D9D9D91A] px-4 py-3 text-sm"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="an"
+                    className="mb-1 block text-sm font-medium"
+                  >
+                    Account Numer
+                  </label>
+                  <input
+                    type="text"
+                    id="an"
+                    className="border-[#00000026 w-full rounded-lg border bg-[#D9D9D91A] px-4 py-3 text-sm"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="tid"
+                    className="mb-1 block text-sm font-medium"
+                  >
+                    Transaction ID
+                  </label>
+                  <input
+                    type="text"
+                    id="tid"
+                    className="border-[#00000026 w-full rounded-lg border bg-[#D9D9D91A] px-4 py-3 text-sm"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="tr"
+                    className="mb-1 block text-sm font-medium"
+                  >
+                    Transaction Receipt
+                  </label>
+                  <input
+                    type="text"
+                    id="tr"
+                    className="border-[#00000026 w-full rounded-lg border bg-[#D9D9D91A] px-4 py-3 text-sm"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="adi"
+                    className="mb-1 block text-sm font-medium"
+                  >
+                    Any additional Information
+                  </label>
+                  <input
+                    type="text"
+                    id="adi"
+                    className="border-[#00000026 w-full rounded-lg border bg-[#D9D9D91A] px-4 py-3 text-sm"
+                  />
+                </div>
+                <div className="flex items-center gap-x-4">
+                  <input type="checkbox" checked id="tac" />
+                  <label htmlFor="tac">
+                    I agree with the terms and condition
+                  </label>
+                </div>
+              </form>
+              <div>
+                <h2 className="mb-2 text-center text-xl font-bold">
+                  Payment Processing
+                </h2>
+                <PaymentInformation
+                  handleClick={() => {
+                    setShowVerifyPaymentModal(false);
+                    setSuccessPaymentModal(true);
+                  }}
+                />
+              </div>
+            </div>
+          </article>
+        </div>
+      )}
+
+      {successPaymentModal && (
+        <div className="absolute left-0 top-0 h-screen w-screen bg-black/50">
+          <article className="absolute left-1/2 top-1/2 max-h-[90vh] -translate-x-1/2 -translate-y-1/2 overflow-y-auto rounded-lg bg-white p-5">
+            <div className="mb-6 grid gap-y-0.5 px-4 text-center">
+              <GiCheckMark className="mx-auto text-4xl font-black text-green-400" />
+              <h3 className="text-2xl font-bold text-green-400">
+                Payment Succesful
+              </h3>
+              <p className="font-semibold text-gray-500">
+                Thank you! Your payment is complete
+              </p>
+            </div>
+            <div className="flex justify-center">
+              <button
+                className="mx-auto rounded bg-green-500 p-1 px-2 text-sm font-medium text-white"
+                onClick={() => setSuccessPaymentModal(false)}
+              >
+                Done
+              </button>
+            </div>
+          </article>
+        </div>
       )}
     </section>
   );
 }
+
+const PaymentInformation = ({ handleClick }: { handleClick: () => void }) => {
+  return (
+    <div className="grid shrink-0 gap-y-4 rounded-lg bg-[#E8E3E3] p-5 shadow-xl shadow-[#e8e3e3]/50">
+      <div>
+        <p>Project amount: 1500 USD</p>
+        <p>Paid Amount: 500 USD</p>
+        <p>Left Amount: 1000 USD</p>
+      </div>
+
+      <div>
+        <p>Pay Amount: 500USD</p>
+        <p>VAT(2%): 10</p>
+        <p>Total: 510 USD</p>
+      </div>
+
+      <button
+        onClick={handleClick}
+        className="rounded-lg bg-[#3C5A99] py-3 text-white"
+      >
+        Continue
+      </button>
+    </div>
+  );
+};
